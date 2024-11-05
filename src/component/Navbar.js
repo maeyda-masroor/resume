5bbdad185b4a4826e663e423b3b8e75e831346e4
maeyda masroor <iosattbsoft2021@gmail.com>
2024-11-05 15:07:30 +0500

    b

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,3 +1,7 @@
 export default function Footer(){
-    return <div>cc</div>
+    return <div>
+        <Link href="/Create_resume">Create resume</Link>
+        <Link href="/test">text</Link>
+        <Link href="/Create_cover_letter">Create resume2</Link>
+    </div>
 }
