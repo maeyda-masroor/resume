c991aaff7a32175bea9c1f678057b9ee0ec09688
maeyda masroor <iosattbsoft2021@gmail.com>
2024-11-05 15:01:02 +0500

    b

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
-
+import Navbar from '../component/Navbar';
+import Footer from '../component/Footer';
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -21,7 +22,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
